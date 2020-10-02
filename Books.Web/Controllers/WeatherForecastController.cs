211620555f64030c8fd7358ede5a1c71b7a9a799
collozy <ozlocollins@gmail.com>
2020-10-02 09:46:55 +0300

    Initial project setup

---
+++ Books.Web/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Books.Web.Controllers
+{
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/api/books")]
+        public ActionResult GetBooks()
+        {
+            return Ok("Books");
+        }
+
+    }
+}
